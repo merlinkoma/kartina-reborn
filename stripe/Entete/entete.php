1d8bfc7de0d8cfb5f593c38d56f865b89c044a47
jordanamr <contact@jordanrey.net>
2021-03-09 12:50:05 +0100

    Ajout de stripe

---
+++ stripe/Entete/entete.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Entete/style.css">
+    <title>TP Yellowkorner</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+
+            <div>
+                <div class="titre">
+
+
+                    <h1><a href="lien pour revenir a l'accueil">KARTINA</a></h1>
+
+                    <p>L'Art au bout des doigts.</p>
+
+
+                </div>
+
+                <div class="barrerecherche">
+
+                    <form action="get">
+                        <input type="search" name="recherche" id="cherche" placeholder="Rechercher sur Kartina"
+                            size="50">
+                    </form>
+
+                </div>
+
+                <div class="info">
+
+                    <div>
+                        <a href="http://127.0.0.1:5500/Aide/Aide.html">Aide</a>
+                    </div>
+
+                    <div>
+                        <form action="">
+                            <img src="../Images/langue.png" alt="langue">
+                            <select name="langue" id="">
+                                <option value="france">FR</option>
+                                <option value="anglais">EN</option>
+                            </select>
+                        </form>
+                    </div>
+
+                    <div>
+                        <a href="lien pour se connecter"><img src="../Images/user.png" alt="user"> Sign-in</a>
+                    </div>
+
+                    <div>
+                        <a href="lien pour aller dans le panier"><img src="../Images/shopping-cart.png" alt="panier">
+                            Panier</a>
+                    </div>
+                </div>
+            </div>
+
+            <div id="trait"></div>
+            <div id="trait"></div>
+            <div class="navigation">
+                <ul>
+                    <li><a href="lien vers les photo">Photographies</a></li>
+                    <li><a href="liens vers les nouveautés">Nouveautés</a></li>
+                    <li><a href="liens vers les artistes">Artistes</a></li>
+                    <li><a href="liens vers les derniers exemplaires">Derniers exemplaires</a></li>
+                </ul>
+            </div>
+
+        </header>
+    </div>
+</body>
+
+</html>
