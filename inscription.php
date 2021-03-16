f49ed02ea4901fffb24f7925eb6f7ccb8adf9fe2
alexandre marillesse <alexandre.marillesse@gmail.com>
2021-03-16 11:15:55 +0100

    mon commit num 2 page création de compte utilisateur

--- inscription.php
+++ inscription.php
@@ -1,17 +1,28 @@
 <?php
 require './partials/header.php';
+if( !empty($_POST)){
+    if (!empty($_POST['name'])&& !empty($_POST['firstname']) && !empty($_POST['age'])&& !empty($_POST['motDePasse'])){
+
+    }
+
+}
 ?>
 <style>
 #form{
     margin-left: 45%;
-    width: 150px;
+    width: 200px;
     display: block;
+    height: auto;
+    width: 100%;
+     font-family: Oswald;
+     font-size: 1.5em;
+    color: #e7e7e7;
     }
-    label{
+label{
         width: 90px;
         display: block;
     }
-    input{
+input{
         width: 100px ;
     }
 </style>
@@ -19,17 +30,17 @@ require './partials/header.php';
 <form action="">
 <div id="form">
 <label>Nom</label>
-<input type="text" required>
+<input type="text" name="name" required>
 <label>Prenom</label>
-<input type="text" required>
+<input type="text" name="firstname" required>
 <label>age</label>
-<input type="number">
+<input type="number" name="age">
 <label>date Naissance</label>
-<input type="date">
+<input type="date" name="birthday">
 <label>Mot de passe</label>
-<input type="password">
+<input type="password" name="motDePasse">
 <label>Repeter mot de passe</label>
-<input type="password">
+<input type="password" name="motDePasse">
 </div>
 </form>
 </body>
