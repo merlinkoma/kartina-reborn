1f375d33900404e6af68e262b73ed7e2f876939c
alexandre marillesse <alexandre.marillesse@gmail.com>
2021-03-18 10:48:45 +0100

    commit creation bdd pour page creation utilisateur

--- inscription.php
+++ inscription.php
@@ -4,7 +4,21 @@ if( !empty($_POST)){
     if (!empty($_POST['name'])&& !empty($_POST['firstname']) && !empty($_POST['age'])&& !empty($_POST['motDePasse'])){
             if(filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
                     echo 'l\'adresse email est validée' ; 
-            }
+                $password=$_POST['motDePasse'];
+                $email=$_POST['email'];
+                $prenom=$_POST['firstname'];
+
+               
+                $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+                $query =$db->prepare('INSERT INTO user (email ,prenom , nom , password) VALUES (:email , :prenom, :nom,:password)');
+                $query->bindValue(':password', password_hash($password,PASSWORD_DEFAULT));
+                $query->bindValue(':email',$email);
+                $query->bindValue(':prenom',$prenom);
+                }
+                else{
+                $errors['email']="l'email n'est pas valide";
+                }
+
     }
 
 }
@@ -31,20 +45,23 @@ input{
 <body>
 <form action="">
 <div id="form">
-<label>Nom</label>
-<input type="text" name="name" required>
-<label>Prenom</label>
-<input type="text" name="firstname" required>
-<label>email</label>
-<input type="email" name="email">
-<label>age</label>
-<input type="number" name="age">
-<label>date Naissance</label>
-<input type="date" name="birthday">
-<label>Mot de passe</label>
-<input type="password" name="motDePasse">
-<label>Repeter mot de passe</label>
-<input type="password" name="motDePasse">
+    <label>Nom</label>
+        <input type="text" name="name" >
+        <label>Prenom</label>
+        <input type="text" name="firstname">
+        <label>email</label>
+        <input type="email" name="email">
+        <label>age</label>
+        <input type="number" name="age">
+        <label>date Naissance</label>
+        <input type="date" name="birthday">
+        <label>Mot de passe</label>
+        <input type="password" name="motDePasse">
+        <label>Repeter mot de passe</label>
+        <input type="password" name="motDePasse">
+        <br>
+        <button type="submit">Envoyer</button>
 </div>
+    
 </form>
 </body>
