534ec410b29358fbf568f36ee4b806f0db707810
Lucas <lucasju@hotmail.fr>
2021-03-25 17:00:03 +0100

    debut BDD panier

--- panier.php
+++ panier.php
@@ -1,9 +1,29 @@
 <?php
+
+ob_start();
+
 $title = 'panier';
 require_once './partials/header.php';
 require_once './partials/ariane.php';
 
+global $db;
+
+$clear = isset($_GET['clear']) ? true : false;
+
+if ($clear) {
+    unset($_SESSION['panier']);
+
+    header('Location: panier.php');
+}
+
+function panier()
+{
+    return $_SESSION['panier'] ?? [];
+}
+
+
 ?>
+
 
 <div class="container-panier">
 
@@ -11,117 +31,125 @@ require_once './partials/ariane.php';
         <p>Votre Panier</p>
     </div>
 
-    <div class="main">
+    <?php if (empty(panier())) { ?>
+        <h2>Votre panier est vide...</h2>
+    <?php } ?>
 
-        <div class="pic">
-            <img src="./assets/icons/try.jpg" alt="photo du panier">
+    <?php if (!empty(panier())) { ?>
+        <div class="main">
+            <?php foreach (panier() as $panier) { ?>
+
+
+                <div class="pic">
+                    <img src="./assets/icons/try.jpg" alt="photo du panier">
+                </div>
+
+                <div class="infos">
+                    <div>
+                        <h2>Nom de l'artiste</h2>
+                    </div>
+                    <div>
+                        <p>Description de la photographie</p>
+                    </div>
+                </div>
+
+                <div class="caracteristiques">
+                    <div>
+                        <div>
+                            <h3>Format</h3>
+                        </div>
+                        <div>
+                            <p>Grand - 60 x 75 cm</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>Finition</h3>
+                        </div>
+                        <div>
+                            <p>SUR PAPIER PHOTO</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>Cadre</h3>
+                        </div>
+                        <div>
+                            <p>Sans encadrement</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="quantiter">
+                    <div>
+                        <h3><label for="quantity">Quantité</label></h3>
+                    </div>
+                    <div>
+                        <input type="number" name="quantite" id="quantity" value="1">
+                    </div>
+                    <div>
+                        <button type="reset"><a href="">Supprimer</a></button>
+                    </div>
+                </div>
+
+                <div class="prix">
+                    <h2 data-prix="110.00" id="calcul">110,00€</h2>
+                </div>
+
         </div>
 
-        <div class="infos">
-            <div>
-                <h2>Nom de l'artiste</h2>
+        <div class="finalisation">
+
+            <div class="back">
+                <button><a href="">&#8249; Continuer mes achats</a></button>
             </div>
-            <div>
-                <p>Description de la photographie</p>
+
+            <div class="payement">
+                <div class="promo">
+                    <input type="checkbox" name="reduc" id="reduction">
+                    <label for="reduction">J'ai une carte cadeau / un code promo</label>
+                </div>
+
+                <div class="pfinal">
+                    <div>
+                        <h3>Total</h3>
+                    </div>
+                    <div class="finalprix">
+                        <h2 id="fcalcul" data-prix="110.00">110,00€</h2>
+                        <p id="tva">dont tva 20% : 22,00€</p>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"><a href="">Valider ma commande</a></button>
+                </div>
+
+                <div class="securise">
+                    <p>Paiement sécurisé</p>
+                </div>
+
+                <div class="typepayement">
+                    <div>
+                        <img src="./assets/icons/credit-card.svg" alt="CB">
+                    </div>
+                    <div>
+                        <img src="./assets/icons/paypal.svg" alt="paypal">
+                    </div>
+                    <div>
+                        <img src="./assets/icons/visa.svg" alt="visa">
+                    </div>
+                    <div>
+                        <img src="./assets/icons/mastercard.svg" alt="MasterCard">
+                    </div>
+                    <div>
+                        <img src="./assets/icons/american-express.svg" alt="AmericanExpress">
+                    </div>
+                </div>
             </div>
+        <?php } ?>
         </div>
 
-        <div class="caracteristiques">
-            <div>
-                <div>
-                    <h3>Format</h3>
-                </div>
-                <div>
-                    <p>Grand - 60 x 75 cm</p>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <h3>Finition</h3>
-                </div>
-                <div>
-                    <p>SUR PAPIER PHOTO</p>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <h3>Cadre</h3>
-                </div>
-                <div>
-                    <p>Sans encadrement</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="quantiter">
-            <div>
-                <h3><label for="quantity">Quantité</label></h3>
-            </div>
-            <div>
-                <input type="number" name="quantite" id="quantity" value="1">
-            </div>
-            <div>
-                <button type="reset"><a href="">Supprimer</a></button>
-            </div>
-        </div>
-
-        <div class="prix">
-            <h2 data-prix="110.00" id="calcul">110,00€</h2>
-        </div>
-
-    </div>
-
-    <div class="finalisation">
-
-        <div class="back">
-            <button><a href="">&#8249; Continuer mes achats</a></button>
-        </div>
-
-        <div class="payement">
-            <div class="promo">
-                <input type="checkbox" name="reduc" id="reduction">
-                <label for="reduction">J'ai une carte cadeau / un code promo</label>
-            </div>
-
-            <div class="pfinal">
-                <div>
-                    <h3>Total</h3>
-                </div>
-                <div class="finalprix">
-                    <h2 id="fcalcul" data-prix="110.00">110,00€</h2>
-                    <p id="tva">dont tva 20% : 22,00€</p>
-                </div>
-            </div>
-
-            <div>
-                <button type="submit"><a href="">Valider ma commande</a></button>
-            </div>
-
-            <div class="securise">
-                <p>Paiement sécurisé</p>
-            </div>
-
-            <div class="typepayement">
-                <div>
-                    <img src="./assets/icons/credit-card.svg" alt="CB">
-                </div>
-                <div>
-                    <img src="./assets/icons/paypal.svg" alt="paypal">
-                </div>
-                <div>
-                    <img src="./assets/icons/visa.svg" alt="visa">
-                </div>
-                <div>
-                    <img src="./assets/icons/mastercard.svg" alt="MasterCard">
-                </div>
-                <div>
-                    <img src="./assets/icons/american-express.svg" alt="AmericanExpress">
-                </div>
-            </div>
-        </div>
-
-    </div>
-
+    <?php } ?>
 </div>
 
 <?php require_once './partials/footer.php'; ?>
